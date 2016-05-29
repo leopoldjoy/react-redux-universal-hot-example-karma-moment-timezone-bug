5aea1552dc9eb3be60b13528f0e76d908fe46408
Leopold Joy <leo@lotsa.ca>
2016-05-29 17:53:22 -0400

    add moment-timezone to InfoBar.js component to show bug when run as a test with Karma

--- src/components/InfoBar/InfoBar.js
+++ src/components/InfoBar/InfoBar.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
+import moment from 'moment-timezone';
 import {load} from 'redux/modules/info';
 
 @connect(
@@ -15,11 +16,13 @@ export default class InfoBar extends Component {
   render() {
     const {info, load} = this.props; // eslint-disable-line no-shadow
     const styles = require('./InfoBar.scss');
+    const userTimezone = moment.tz.guess();
     return (
       <div className={styles.infoBar + ' well'}>
         <div className="container">
           This is an info bar
           {' '}
+          You are in {userTimezone}
           <strong>{info ? info.message : 'no info!'}</strong>
           <span className={styles.time}>{info && new Date(info.time).toString()}</span>
           <button className="btn btn-primary" onClick={load}>Reload from server</button>
